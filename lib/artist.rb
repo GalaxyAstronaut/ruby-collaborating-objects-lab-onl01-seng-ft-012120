9db050ad1326c403feb3e5616acde539086b2307
Oscar Zapata <0northbridge.cpu1@gmail.com>
2020-02-24 19:43:38 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,11 +13,7 @@ class Artist
     @@all
   end
 
-  def self.create(name)
-    artist = self.new(name)
-    artist.name = name
-    artist
-  end
+ 
 
   def self.find(name)
     self.all.detect { |artist| artist.name == name }
